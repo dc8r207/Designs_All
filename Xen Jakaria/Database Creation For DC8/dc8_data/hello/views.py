a7a10ee76ab06cd8b14c7af864c08faa6f746f2d
jakariapervez <jakariapervez@gmail.com>
2023-09-22 05:25:59 +0600

    mycommit
    mycommit

--- Xen Jakaria/Database Creation For DC8/dc8_data/hello/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-def home(request):
-    return HttpResponse("Welcome to Design Circele 8")
-
-# Create your views here.
--- Xen Jakaria/Database Creation For DC8/dc8_data_office/dc8_jurisdiction/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Dc8JurisdictionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dc8_jurisdiction'
