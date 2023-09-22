7d5088594be108c90b9215cac1ab0db964a01006
jakariapervez <jakariapervez@gmail.com>
2023-09-22 12:30:48 +0600

    jkkffkkf
    jkkffkkf

--- Xen Jakaria/Database Creation For DC8/dc8_data_office/dc8_progress_management/settings.py
+++ Xen Jakaria/Database Creation For DC8/dc8_data_office/dc8_progress_management/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+import os
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
